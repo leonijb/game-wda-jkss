513eba6ce9b74ee61d44d9e486dafb2a042608e7
leonijb <116459301+leonijb@users.noreply.github.com>
2023-03-29 17:35:04 +0300

    Форма игры

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    ship.move(-1)
+})
+input.onButtonPressed(Button.B, function () {
+    ship.move(1)
+})
+let ship: game.LedSprite = null
+ship = game.createSprite(2, 4)
 basic.forever(function () {
 	
 })
