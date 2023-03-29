9b4eef8d3bb2373e753f9f3f12fa786f3f14a7d2
leonijb <116459301+leonijb@users.noreply.github.com>
2023-03-29 17:40:32 +0300

    Заряжаем

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
 input.onButtonPressed(Button.A, function () {
     ship.move(-1)
 })
+input.onButtonPressed(Button.AB, function () {
+    shoot = game.createSprite(ship.get(LedSpriteProperty.X), ship.get(LedSpriteProperty.X))
+})
 input.onButtonPressed(Button.B, function () {
     ship.move(1)
 })
+let shoot: game.LedSprite = null
 let ship: game.LedSprite = null
 ship = game.createSprite(2, 4)
-basic.forever(function () {
-	
-})
